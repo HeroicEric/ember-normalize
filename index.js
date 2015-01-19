a3c2b13b9e51fa065a4f44b8e003cbb63a8115e2
Eric Kelly <heroiceric@gmail.com>
2015-01-19 16:01:43 -0500

    Magically import normalize.css via bower

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-normalize'
+  name: 'ember-cli-normalize',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/normalize.css/normalize.css');
+  }
 };
