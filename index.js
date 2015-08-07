22b45df364b71d99547437a333710a788ed6c031
Eric Kelly <heroiceric@gmail.com>
2015-08-06 21:05:15 -0400

    Import normalize.css without specifying bower_components path

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+
 module.exports = {
-  name: 'ember-normalize'
+  name: 'ember-normalize',
+
+  treeForStyles: function() {
+    var normalizePath = path.join(this.app.bowerDirectory, 'normalize.css');
+    var normalizeTree = new Funnel(this.treeGenerator(normalizePath), {
+      srcDir: '/',
+      destDir: '/app/styles'
+    });
+
+    return normalizeTree;
+  }
 };
